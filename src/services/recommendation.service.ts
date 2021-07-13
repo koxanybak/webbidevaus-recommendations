d4f443dd332ca0bb43507734a69a118bb87047d0
koxanybak <leevi.heikkila11@gmail.com>
2021-07-13 20:07:52 +0300

    fix reco parsing

--- src/services/recommendation.service.ts
+++ src/services/recommendation.service.ts
@@ -13,7 +13,7 @@ export const getAllRecommendations = async () => {
         // This little "feature" ate like 3 hours of my time :(
         // For more information: https://stackoverflow.com/questions/11477415/why-does-javascripts-regex-exec-not-always-return-the-same-value
         const recoSectionRegex = /jakson\svalinnat:?[\n\s\t\r*-]+(.+?)(?=$|##)/gis;
-        const recosAndAuthorRegex = /(\w+):[\n\s*-]+(.+?)(?=$|[\n\s*-]+\w+:)/gis;
+        const recosAndAuthorRegex = /([\wÀ-ž]+):[\n\s*-]+(.+?)(?=$|[\n\s*-]+[\wÀ-ž]+:(?![^[]*\]))/gis;
 
         // Get the recommendation section
         const recoSectionMatch = recoSectionRegex.exec(episode.long_description);
@@ -26,6 +26,9 @@ export const getAllRecommendations = async () => {
         // Extract the authors and the recommendations
         // NOTE: (Two recommendations made by the same author will be classified as one recommendation.)
         const recoAndAuthorMatchs = [...recoSection.matchAll(recosAndAuthorRegex)];
+        // if (episode.number === 9) {
+        //     return;
+        // }
         if (recoAndAuthorMatchs.length === 0) {
             console.error(`Couldn't find recommendations or authors for episode ${episode.number}`);
             return;
