d5fb38e38155464f8d2117f00f8ccc2429a443b5
koxanybak <leevi.heikkila11@gmail.com>
2021-07-14 11:22:03 +0300

    add parsing documentation

--- src/services/recommendation.service.ts
+++ src/services/recommendation.service.ts
@@ -2,15 +2,17 @@ import axios from "axios";
 import { Episode } from "../types/Episode";
 import { Recommendation } from "../types/Recommendation";
 
-
+/**
+ * Gets all episodes from the webbidevaus.fi server and parses and returns the recommendations.
+ */
 export const getAllRecommendations = async () => {
     const { data: episodes } = await axios.get("https://webbidevaus.fi/episodes.json");
 
     // Parse recommendations out of the episodes
-    const recos: Recommendation[] = [];
+    const recommendations: Recommendation[] = [];
     episodes.forEach((episode: Episode) => {
-        // The regexes need to be declared in again for every iteration.
-        // This little "feature" ate like 3 hours of my time :(
+        // The regexes need to be declared again for every iteration.
+        // This little "feature" ate about 3 hours of my time :(
         // For more information: https://stackoverflow.com/questions/11477415/why-does-javascripts-regex-exec-not-always-return-the-same-value
         const recoSectionRegex = /jakson\svalinnat:?[\n\s\t\r*-]+(.+?)(?=$|##)/gis;
         const recosAndAuthorRegex = /([\wÀ-ž]+):[\n\s*-]+(.+?)(?=$|[\n\s*-]+[\wÀ-ž]+:(?![^[]*\]))/gis;
@@ -26,15 +28,12 @@ export const getAllRecommendations = async () => {
         // Extract the authors and the recommendations
         // NOTE: (Two recommendations made by the same author will be classified as one recommendation.)
         const recoAndAuthorMatchs = [...recoSection.matchAll(recosAndAuthorRegex)];
-        // if (episode.number === 9) {
-        //     return;
-        // }
         if (recoAndAuthorMatchs.length === 0) {
             console.error(`Couldn't find recommendations or authors for episode ${episode.number}`);
             return;
         }
         recoAndAuthorMatchs.forEach((match) => {
-            recos.push({
+            recommendations.push({
                 episodeNumber: episode.number,
                 author: match[1],
                 recommendation: match[2],
@@ -43,5 +42,5 @@ export const getAllRecommendations = async () => {
         
     });
 
-    return recos;
+    return recommendations;
 };
